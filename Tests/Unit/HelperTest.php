3aef90b4e35187b3767b8d6787ef747b22dbe772
Stefan Horst <stefan.horst@grandcentrix.net>
2017-04-18 10:28:36 +0200

    added more converter tests

--- Tests/Unit/HelperTest.php
+++ Tests/Unit/HelperTest.php
@@ -5,6 +5,7 @@ namespace ScoutNet\Api\Tests;
 use PHPUnit\Framework\TestCase;
 use ScoutNet\Api\Helpers\CacheHelper;
 use ScoutNet\Api\Helpers\ConverterHelper;
+use ScoutNet\Api\Models\Categorie;
 use ScoutNet\Api\Models\Event;
 
 class CacheHelperTest extends TestCase {
@@ -39,4 +40,50 @@ class ConvertHelperTest extends TestCase {
         $this->assertInstanceOf(ConverterHelper::class, new ConverterHelper());
     }
 
+    public function testConvertCategorieValidArray() {
+        $converter = new ConverterHelper();
+
+        $expected_categorie = new Categorie();
+
+        $expected_categorie->setUid(23);
+        $expected_categorie->setText('Categorie 1');
+        $expected_categorie->setAvailable(false);
+
+        $array = ['ID'=> 23, 'Text' => 'Categorie 1'];
+        $is_categorie = $converter->convertApiToCategorie($array);
+
+        $this->assertEquals($expected_categorie, $is_categorie);
+    }
+
+    public function testConvertCategorieEmptyArray() {
+        $converter = new ConverterHelper();
+
+        $expected_categorie = new Categorie();
+
+        $expected_categorie->setUid(-1);
+        $expected_categorie->setText('');
+        $expected_categorie->setAvailable(false);
+
+        $array = [];
+        $is_categorie = $converter->convertApiToCategorie($array);
+
+        $this->assertEquals($expected_categorie, $is_categorie);
+    }
+
+    public function testConvertCategorieValidArrayOverCache() {
+        $cache = new CacheHelper();
+        $converter = new ConverterHelper($cache);
+
+        $expected_categorie = new Categorie();
+
+        $expected_categorie->setUid(23);
+        $expected_categorie->setText('Categorie 1');
+        $expected_categorie->setAvailable(false);
+
+        $array = ['ID'=> 23, 'Text' => 'Categorie 1'];
+        $converter->convertApiToCategorie($array);
+        $cached_categorie = $cache->get(Categorie::class, 23);
+
+        $this->assertEquals($expected_categorie, $cached_categorie);
+    }
 }
